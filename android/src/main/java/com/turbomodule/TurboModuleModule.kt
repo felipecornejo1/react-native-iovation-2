e4d64b6169a9a7ddf417e51b16e9e92c20817892
Felipe Cornejo <felicornejo@gmail.com>
2024-06-21 16:40:33 -0300

    chore: setup

--- android/src/main/java/com/turbomodule/TurboModuleModule.kt
+++ android/src/main/java/com/turbomodule/TurboModuleModule.kt
@@ -1,23 +1,44 @@
 package com.turbomodule
 
 import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.module.annotations.ReactModule
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.Promise
+import com.iovation.mobile.android.FraudForceConfiguration
+import com.iovation.mobile.android.FraudForceManager
 
-@ReactModule(name = TurboModuleModule.NAME)
 class TurboModuleModule(reactContext: ReactApplicationContext) :
-  NativeTurboModuleSpec(reactContext) {
+  ReactContextBaseJavaModule(reactContext) {
 
   override fun getName(): String {
     return NAME
   }
 
-  // Example method
-  // See https://reactnative.dev/docs/native-modules-android
-  override fun multiply(a: Double, b: Double): Double {
-    return a * b
+  @ReactMethod
+  fun multiply(a: Double, b: Double, promise: Promise) {
+    promise.resolve(a * b)
+  }
+
+  val configuration = FraudForceConfiguration.Builder()
+	  .subscriberKey("M1WrRSwcjUBQmHamij3DxQJWr00YzfRhXaMkI+zhhiY=")
+		.enableNetworkCalls(true)
+		.build()
+
+  val context = reactContext.getApplicationContext()
+
+	init {
+	  FraudForceManager.initialize(configuration, context)
+    FraudForceManager.refresh(context)
+	}
+
+  @ReactMethod
+  fun getBlackbox(a: Double, b: Double, promise: Promise) {
+    val blackbox = FraudForceManager.getBlackbox(context)
+    promise.resolve(blackbox)
   }
 
   companion object {
     const val NAME = "TurboModule"
   }
 }
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,9 @@
 const TurboModule = require('./NativeTurboModule').default;
 
-export function multiply(a: number, b: number): number {
+export function multiply(a: number, b: number): Promise<number> {
   return TurboModule.multiply(a, b);
+}
+
+export function getBlackbox(): Promise<string> {
+  return TurboModule.getBlackbox(1, 1);
 }
